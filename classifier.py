8aee69a0248d56c6544b2ab0fe93267fa1ec0223
Samvit Jatia <samvit@hotmail.com>
2024-04-15 23:13:47 +0530

    final classifier

--- classifier.py
+++ classifier.py
@@ -4,33 +4,25 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.linear_model import LogisticRegression
-# Handle SSL Certificate Verification for NLTK on macOS
-# import ssl
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
 
-# nltk.download('stopwords')
-
-
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
+# Initialize the lemmatizer and stopwords list
 lemmatizer = WordNetLemmatizer()
-
-
 stop_words = set(stopwords.words('english'))
 
-# Load the dataset into a pandas DataFrame
+# Path to the dataset
 file_path = './mbti_1.csv'
 
+# List of valid MBTI types
+valid_mbti_types = {
+    'ISTJ', 'ISFJ', 'INFJ', 'INTJ', 'ISTP', 'ISFP', 'INFP', 'INTP',
+    'ESTP', 'ESFP', 'ENFP', 'ENTP', 'ESTJ', 'ESFJ', 'ENFJ', 'ENTJ'
+}
+
 def load_data(file_path):
+    """ Load data from a CSV file, filtering for valid MBTI types. """
     print('\nLoading data from csv...')
     types = []
     posts = []
@@ -49,77 +41,42 @@ def load_data(file_path):
     return df
 
 def clean_data(df):
-    print('\n')
-    print('Cleaning data...')
-    #removing urls
-    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'http\S+', '', x))
-    #converting to lowercase
-    df['Posts'] = df['Posts'].apply(lambda x: x.lower())
-
-    #remove punctuation, numbers and whitespace
-    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
-    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'\d+', '', x))
-    df['Posts'] = df['Posts'].apply(lambda x: x.strip())
-    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'\s+', ' ', x))
-
-    # #removing stopwords
-    stop_words = set(stopwords.words('english'))
-    df['Posts'] = df['Posts'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
-    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'[^\x00-\x7F]+', ' ', x))
-    
-    #lemmatizing is taking time
-    df['Posts'] = df['Posts'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
+    """ Clean text data in the dataframe. """
+    print('\nCleaning data...')
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'http\S+', '', x))  # Removing URLs
+    df['Posts'] = df['Posts'].apply(lambda x: x.lower())  # Converting to lowercase
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'[^\w\s]', '', x))  # Remove punctuation
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'\d+', '', x))  # Remove digits
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'\s+', ' ', x).strip())  # Remove extra spaces
+    df['Posts'] = df['Posts'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))  # Removing stopwords
+    df['Posts'] = df['Posts'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))  # Lemmatizing
     print('Data cleaning completed')
     return df
 
-valid_mbti_types = {'ISTJ', 'ISFJ', 'INFJ', 'INTJ', 'ISTP', 'ISFP', 'INFP', 'INTP',
-                    'ESTP', 'ESFP', 'ENFP', 'ENTP', 'ESTJ', 'ESFJ', 'ENFJ', 'ENTJ'}
-
 df = clean_data(load_data(file_path))
-unique_types = df['Type'].unique()
-
-# Print the unique values found in the 'Type' column
-print("Unique MBTI Types in the DataFrame:")
-print(unique_types)
 
 x = df['Posts']
 y = df['Type']
-print('\n')
-print('Splitting dataset into train-test...')  
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
-print("Splitting complete.")
-
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.80, random_state=100)
+print("\nSplitting complete.")
 
 def model_LR():
-    print('\n')
-    print('!-----!-----!-----!-----!-----!-----!-----!-----!-----!-----!-----!-----!')
-    print('Applying LR model...')
-    print('!-----!-----!-----!-----!-----!-----!-----!-----!-----!-----!-----!-----!')
+    """ Train and evaluate a logistic regression model. """
+    print('\nApplying LR model...')
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, min_df=2, max_features=10000, ngram_range=(1, 2))
-    # Fit and transform the training data
-    X_train_tfidf = vectorizer.fit_transform(X_train)
-    X_test_tfidf = vectorizer.transform(X_test)
+    X_train_tfidf = vectorizer.fit_transform(X_train)  # Transform training data
+    X_test_tfidf = vectorizer.transform(X_test)  # Transform test data
 
-    print('\n')
-    print('Loading model...')
-    # Initialize Logistic Regression
+    print('\nLoading model...')
     log_reg = LogisticRegression(C=1.0, max_iter=1000, solver='saga', penalty='l1')
-
     print("Model loaded.")
 
-    # Train
-    print('\n')
-    print('Training...')
+    print('\nTraining...')
     log_reg.fit(X_train_tfidf, y_train)
     print('Trained.')
 
-    # Predict and evaluate
     y_pred = log_reg.predict(X_test_tfidf)
-
-    # Performance
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print(classification_report(y_test, y_pred))
 
-print('\n')
 model_LR()
-
