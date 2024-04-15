d831a91e7ea1cd16d28a3a980f3fedbd855e6f68
Samvit Jatia <samvit@hotmail.com>
2024-04-15 16:07:11 +0530

    Loaded and cleaned the data

--- classifier.py
+++ classifier.py
@@ -1,4 +1,27 @@
 import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Handle SSL Certificate Verification for NLTK on macOS
+# import ssl
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+
+# nltk.download('stopwords')
+
+
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+lemmatizer = WordNetLemmatizer()
+
+
+stop_words = set(stopwords.words('english'))
 
 # Load the dataset into a pandas DataFrame
 file_path = '/Users/admin/NLP_assignment3/mbti_1.csv'
@@ -27,5 +50,25 @@ def load_data(file_path):
     
     return df
 
-df = load_data(file_path)
+def clean_data(df):
+    #removing urls
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'http\S+', '', x))
+    #converting to lowercase
+    df['Posts'] = df['Posts'].apply(lambda x: x.lower())
+
+    #remove punctuation, numbers and whitespace
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'\d+', '', x))
+    df['Posts'] = df['Posts'].apply(lambda x: x.strip())
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'\s+', ' ', x))
+
+    # #removing stopwords
+    stop_words = set(stopwords.words('english'))
+    df['Posts'] = df['Posts'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
+    df['Posts'] = df['Posts'].apply(lambda x: re.sub(r'[^\x00-\x7F]+', ' ', x))
+    
+    #lemmatizing is taking time
+    # df['Posts'] = df['Posts'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
+    return df
+df = clean_data(load_data(file_path))
 print(df.head())
